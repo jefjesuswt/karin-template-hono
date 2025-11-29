43f236f0ab736bb9361dbbd340ebc4bc7c0fd2f2
Jeffrey Jesus <jefjesuswt@gmail.com>
2025-11-28 21:13:00 -0400

    .

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
-import { KarinFactory } from "@karin-js/core";
+import { KarinFactory, Logger } from "@karin-js/core";
 import { HonoAdapter } from "@karin-js/platform-hono";
 
 async function bootstrap() {
+  const logger = new Logger("Bootstrap");
+  const port = 3000;
+
   const app = await KarinFactory.create(new HonoAdapter(), {
     scan: "./src/**/*.controller.ts",
   });
 
-  app.listen(3000);
+  app.listen(port, () => {
+    logger.info(`Karin now listening on port ${port}! 🚀`);
+  });
 }
 
 bootstrap();
